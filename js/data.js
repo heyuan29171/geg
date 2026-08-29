4ab01fceb82ecd045b8651c1c7de3847fa4cf5a1
GEG <geg@example.com>
2026-08-30 01:48:35 +0800

    v2.1.1

--- js/data.js
+++ js/data.js
@@ -157,7 +157,7 @@ const CARD_MAP = {};
 CARDS.forEach(c => { CARD_MAP[c.id] = c; });
 
 const CONFIG = {
-  VERSION: '2.1.0',
+  VERSION: '2.1.1',
   INTERVAL_BASE: 10,
   INTERVAL_EXP: 0.4956,
   MIN_INTERVAL: 0.005,
